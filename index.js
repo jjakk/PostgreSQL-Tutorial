3904b7775217dc740c0c4d0b880bfa1ae524bc3d
John Khachian <jaksawesome10@gmail.com>
2021-11-17 00:31:31 -0500

    woops

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const studentRoutes = require('./src/student/routes');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+require("dotenv").config();
 app.use(express.json());
 
 app.get('/', (req, res) => {
