b9a883aef702deb815991bc3037fe813b9d3034d
John Khachian <jaksawesome10@gmail.com>
2021-11-17 00:25:42 -0500

    maybe this will work

--- db.js
+++ db.js
@@ -1,11 +1,5 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATBASE_HOST,
-    database: process.env.DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.DATABASE_PORT
-});
+const pool = new Pool();
 
 module.exports = pool;
