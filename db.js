b0853c80d5beb341318f5d317efe15a21d09cc79
John Khachian <jaksawesome10@gmail.com>
2021-11-17 00:17:31 -0500

    environment variables

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'students',
-    password: process.env.POSTGRES_PW
+    user: process.env.DATABASE_USER,
+    host: process.env.DATBASE_HOST,
+    database: process.env.DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.DATABASE_PORT
 });
 
 module.exports = pool;
